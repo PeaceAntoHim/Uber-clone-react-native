82a3d41f60824cd0595a0dbda83fe2f5b4aec449
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-03-27 23:51:09 +0700

    finish added redux components

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
+import { store } from "./store";
+import { Provider } from "react-redux";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Provider } from "react-redux";
-
-// 1). Set up redux
 
 export default function App() {
   return (
-    <Provider >
+    <Provider store={store}>
       <View style={styles.container}>
         <Text>Lets build UBER</Text>
         <StatusBar style="auto" />
--- slices/navSlice.js
+++ slices/navSlice.js
@@ -7,6 +7,29 @@ const initialState = {
 };
 
 
+// Create nav layer
 export const navSlice = createSlice({
-   
+   name: 'nav',
+   initialState,
+   reducer: {
+      setOrigin: (state, action) => {
+         state.origin = action.payload;
+      },
+      setDestination: (state, action) => {
+         state.destination = action.payload;
+      },
+      setTravelTimeInformation: (state, action) => {
+         state.travelTimeInformation = action.payload;
+      },
+   },
 });
+
+// Disctructuring all data
+export const { setOrigin, setDestination, setTravelTimeInformation } = navSlice.actions;
+
+// Selectors
+export const selectOrigin = (state) => state.nav.origin;
+export const selectDestination = (state) => state.nav.destination;
+export const selectTravelTimeInformation = (state) => state.nav.travelTimeInformation;
+
+export default navSlice.reducer;
