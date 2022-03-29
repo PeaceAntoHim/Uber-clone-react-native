694d62048779a4439360c9bbf4ffd92c6671fe6e
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-03-29 22:44:05 +0700

    buat components

--- App.js
+++ App.js
@@ -2,14 +2,12 @@ import { store } from "./store";
 import { Provider } from "react-redux";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from "./screens/HomeScreen";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Lets build UBER</Text>
-        <StatusBar style="auto" />
-      </View>
+      <HomeScreen />
     </Provider>
   );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,28 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
+import tw from 'tailwind-react-native-classnames';
 
 const HomeScreen = () => {
    return (
+      <SafeAreaView style={tw`bg-white h-full`}>
          <View>
-            <Text></Text>
+            <Image 
+               style={{ 
+                  width: 100,
+                  height: 100,
+                  resizeMode: "contain",
+               }}
+               source={{ 
+                  uri: "https://links.papareact.com/gzs",
+               }}
+            />
          </View>
+      </SafeAreaView>
    )
 }
 
 export default HomeScreen;
 
-const StyleSheet = StyleSheet.create({
-   
+const styles = StyleSheet.create({
+
 })
