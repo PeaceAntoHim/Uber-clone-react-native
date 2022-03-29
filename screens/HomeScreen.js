34b0cb28cccfb77fd0ee68bbc09f9a8b3b7794d2
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-03-29 22:22:29 +0700

    buat home screen

---
+++ screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const HomeScreen = () => {
+   return (
+         <View>
+            <Text></Text>
+         </View>
+   )
+}
+
+export default HomeScreen;
+
+const StyleSheet = StyleSheet.create({
+   
+})
