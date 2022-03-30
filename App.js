dc8042d686e8caacae3bd24d30af0a75091527a2
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-03-30 22:05:00 +0700

    added react navigation

--- App.js
+++ App.js
@@ -3,14 +3,21 @@ import { store } from "./store";
 import { Provider } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
 import { StyleSheet, Text, View } from 'react-native';
-import{ SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export default function App() {
+  const Stack = createStackNavigator();
+  
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <HomeScreen />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <HomeScreen />
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
