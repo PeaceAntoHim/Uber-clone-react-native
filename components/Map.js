5e30ea15c5283bf9f6b26d26eaa0cbfc1f9b55ba
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-03-31 11:48:02 +0700

    building map components

---
+++ components/Map.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Map = () => {
+   return (
+      <View>
+         <Text>This Map</Text>
+      </View>
+   )
+}
+
+export default Map;
