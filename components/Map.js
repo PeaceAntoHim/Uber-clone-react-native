3e4f113f4fae937f80b8082c898982ed6d1e78fc
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-03-31 11:57:15 +0700

    tambahin mapViwe

--- components/Map.js
+++ components/Map.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import tw from 'tailwind-react-native-classnames';
 
 const Map = () => {
    return (
-      <View>
-         <Text>This Map</Text>
-      </View>
-   )
+      <MapView 
+         style={tw`flex-1`}
+         initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+         }} 
+      />
+   );
 }
 
 export default Map;
